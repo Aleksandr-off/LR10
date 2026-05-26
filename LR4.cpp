2831cc3ae6ccfc88666abfd0d14d5ffeaca31ffc
Студент <student@BT.local>
2026-05-26 15:34:15 +0300

    Добавление файла программирование

---
+++ LR4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Vvedite chislo: ";
+    cin >> a;
+    
+    if (a > 0) {
+        cout << "Chislo polozhitelnoe" << endl;
+    } else if (a < 0) {
+        cout << "Chislo otricatelnoe" << endl;
+    } else {
+        cout << "Eto nol" << endl;
+    }
+    return 0;
+}
